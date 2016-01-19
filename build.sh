0e820da70f37b72177fdee1995ae409f0d33b2fe
Сосна Евгений <shenja@sosna.zp.ua>
2016-01-19 18:26:40 +0300

    Исправления для инициализации проекта.

--- build.sh
+++ build.sh
@@ -35,16 +35,19 @@ echo ""
 echo ""
 echo "Removing old images... "
 
-rmi onec/32bit/baseimage:latest
-rmi onec/baseimage:latest
-echo "Building baseimage i368"
-docker build -q -f Dockerfile.i386 --no-cache -t onec/32bit/baseimage:latest .
+#rmi onec/32bit/baseimage:latest
+#rmi onec/baseimage:latest
 
-echo "Building baseimage amd64"
-docker build -q -f Dockerfile.amd64 --no-cache -t onec/baseimage:latest .
+echo "Building baseimage i368"
+docker build -q -f Dockerfile.i386  -t onec/32bit/baseimage:latest .
 
 echo "Building baseimage for client i386"
-docker build -q -f Dockerfileclient.i386 --no-cache -t onec/32bit/baseimage:latest .
+docker build -q -f Dockerfileclient.i386 -t onec/32bit/baseclient:latest .
+
+#--no-cache
+echo "Building baseimage amd64"
+#docker build -q -f Dockerfile.amd64 -t onec/baseimage:latest .
+
 
 echo ""
 echo "Done!"
